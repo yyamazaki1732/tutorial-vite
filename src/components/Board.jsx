3513c23db7036da8c39ea566dc2818cd60312341
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-05-14 20:44:33 +0900

    update: Finish the unit before [Adding time travel]

---
+++ src/components/Board.jsx
@@ -0,0 +1,87 @@
+import classes from "./index.module.scss";
+import { useState } from "react";
+import { Square } from "./Square";
+
+export const Board = () => {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  // ===============================================
+  // # switch
+  // ===============================================
+  function handleClick(i) {
+    console.log("squares", squares);
+
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  // ===============================================
+  // # calculateWinner
+  // ===============================================
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  // ===============================================
+  // # sign
+  // ===============================================
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <>
+      <div className={classes.status}>{status}</div>
+      <div className={classes.board_row}>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className={classes.board_row}>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className={classes.board_row}>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+};
---
+++ src/components/Game.jsx
@@ -0,0 +1,26 @@
+// import classes from "./index.module.scss";
+// import { useState } from "react";
+// // import { Square } from "./Square";
+
+// import { Board } from "./Board";
+
+// export default function Game() {
+//   const [xIsNext, setXIsNext] = useState(true);
+//   const [history, setHistory] = useState([Array(9).fill(null)]);
+//   const currentSquares = history[history.length - 1];
+
+//   function handlePlay(nextSquares) {
+//     // TODO
+//   }
+
+//   return (
+//     <div className={classes.game}>
+//       <div>
+//         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+//       </div>
+//       <div className={classes.game_info}>
+//         <ol>{/*TODO*/}</ol>
+//       </div>
+//     </div>
+//   );
+// }
